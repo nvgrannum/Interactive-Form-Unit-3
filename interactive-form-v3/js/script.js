50d087cf8db23bddd6021216cc15195dee89f583
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-03-29 15:34:43 -0700

    shirt color update

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -27,21 +27,20 @@ and adjusts the color selection accordingly.
 */
 
 const design = document.querySelector('[name="user-design"]');
-const divShirtColor = document.querySelector('div[id="shirt-colors"]');
-const jsPunShirt = document.querySelectorAll('#shirt-colors option[data-theme="js puns"]');
-const heartJsShirt = document.querySelectorAll('#shirt-colors option[data-theme="heart js"]');
+const divShirtColor = document.getElementById('color');
+const jsPunShirt = document.querySelectorAll('#shirt-colors [data-theme="js puns"]');
+const heartJsShirt = document.querySelectorAll('#shirt-colors [data-theme="heart js"]');
 
-divShirtColor.hidden=true;
+divShirtColor.disabled=true;
 
 design.addEventListener('change', (e) =>{
+    divShirtColor.disabled=false;
     if (e.target.value=='js puns') {
-        divShirtColor.hidden=false;
         for (let i=0, j=0; i<heartJsShirt.length, j<jsPunShirt.length; i++, j++){
             heartJsShirt[i].hidden=true;
             jsPunShirt[j].hidden=false;
         }
     } else if (e.target.value=='heart js') {
-        divShirtColor.hidden=false;
         for (let i=0, j=0; i<heartJsShirt.length, j<jsPunShirt.length; i++, j++){
             heartJsShirt[i].hidden=false;
             jsPunShirt[j].hidden=true;
@@ -50,27 +49,56 @@ design.addEventListener('change', (e) =>{
 });
 
 /*
-Register for activities.
+Register for activities. As the checkboxes are selected and deselected, the event
+price adds and subtracts, respectively, from the event total.
 */
 
 let total = document.querySelector('p#activities-cost');
-console.log(total);
 const eventRegister = document.querySelector('div#activities-box');
-console.log(eventRegister);
-let eventSum = 0;
-
+let eventSum = 0; 
 
 eventRegister.addEventListener('change', (e)=> {
     if(e.target.type=="checkbox") {
-        let dataCost = e.target.querySelector('data-cost').value;
+        let dataCost = e.target.getAttribute('data-cost');
+        let eventDate = e.target.getAttribute('data-day-and-time');
         if(e.target.checked) {
-            console.log(dataCost);
-            eventSum += dataCost;
-        } else if (e.target.checked = false) {
-            eventSum = eventSum - dataCost;
+            eventSum += +dataCost;
+        } else {
+            eventSum -= +dataCost;
         }
-        total.textContent = `Total:$${eventSum}`
-        console.log(total);
+        total.textContent = `Total: $${eventSum}`
+    }
+});
+
+/*
+Payment info section.
+*/
+const paymentInfo = document.querySelector('[name="user-payment"]');
+
+const creditCardSelected = document.querySelector('#payment option[value="credit-card"]')
+const creditCardInfo = document.querySelector('div#credit-card');
+
+const paypalSelected = document.querySelector('#payment option[value="paypal"]')
+const paypalInfo = document.querySelector('div#paypal');
+paypalInfo.hidden=true;
+
+const bitcoinSelected = document.querySelector('#payment option[value="bitcoin"]')
+const bitcoinInfo = document.querySelector('div#bitcoin');
+bitcoinInfo.hidden=true;
+
+paymentInfo.addEventListener('change', (e)=> {
+    if (e.target.value == "paypal") {
+        creditCardInfo.hidden=true;
+        bitcoinInfo.hidden=true;
+        paypalInfo.hidden=false;
+    } else if (e.target.value == 'bitcoin') {
+        creditCardInfo.hidden=true;
+        bitcoinInfo.hidden=false;
+        paypalInfo.hidden=true;
+    } else {
+        creditCardInfo.hidden=false;
+        bitcoinInfo.hidden=true;
+        paypalInfo.hidden=true;
     }
 });
 
